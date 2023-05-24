d9575a94d7972e43c3e0ff147eaaa19bdddd4455
sree <sree@MNM>
2023-05-24 12:35:12 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayBill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Console.WriteLine("Enter the bill amount?");
+            i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your bill is: " + i * 25);
+            Console.Read();
+        }
+    }
+}
